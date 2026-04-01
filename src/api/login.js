e42d4b00322bc9df0c78c2bdf83f6485cbbb8917
kenanqafarov <qafarovkenan06@gmail.com>
2026-04-01 14:15:33 +0400

    Predevelopment checking

--- src/api/login.js
+++ src/api/login.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API_BASE_URL =
-  import.meta.env.VITE_BASE_URL || "http://localhost:5555/api/v1";
+  import.meta.env.VITE_BASE_URL;
 
 export const login = async ({ username, password }) => {
   try {
